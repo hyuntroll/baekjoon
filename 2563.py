23c17d9156729578acf318ee6634bab1b2663ee9
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-21 14:05:14 +0900

    solve problems

--- 2563.py
+++ 2563.py
@@ -1,4 +1,23 @@
 N = int(input())
 
+lst_ld = []
+lst_rp = []
+
 for i in range(N):
-    pass
+    left, down = map(int, input().split())
+    lst_ld.append((left, down))
+    lst_rp.append((left+10, down +10))
+
+# for i in range(N):
+#     for k in range(N):
+#         if lst_ld[i][0] < lst_ld[k][0] < lst_rp[i][0]:
+#             if lst_ld[i][1] < lst_ld[k][1] < lst_rp[i][1]:
+#                 # print(lst_ld[k])
+#             if lst_ld[i][1] < lst_rp[k][1] < lst_rp[i][1]:
+#                 # print(lst_ld[k], "b", i, k)
+#         if lst_ld[i][0] < lst_rp[k][0] < lst_rp[i][0]:
+#             if lst_ld[i][1] < lst_ld[k][1] < lst_rp[i][1]:
+#                 # print(lst_ld[k], "c", i, k)
+
+#             if lst_ld[i][1] < lst_rp[k][1] < lst_rp[i][1]:
+#                 # print(lst_ld[k])
--- 26099.py
+++ 26099.py
@@ -7,13 +7,30 @@ N = int(input())
 
 
 def test(n):
-    ans = []
+    # ans = []
     if n % 5 == 0:
         return n // 5
-    for i in range(0, n//5 + 1):
+    for i in range(n//5, -1, -1):
         if ( N - i * 5 ) % 3 == 0:
-            ans.append(( N - i * 5) // 3 + i )
+            return ( N - i * 5) // 3 + i
+    # for i in range(n//5+1, 0):
     
-    return min(ans) if ans else -1
+    return -1
 
 print(test(N))
+
+
+# 다른 풀의
+
+cnt = 0
+
+while N > 0:
+    if N % 5 == 0:
+        cnt += N // 5
+        print(cnt)
+        break
+    
+    N -= 3
+    cnt += 1
+else:
+    print(-1)
