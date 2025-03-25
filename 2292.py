82f3650d167a59c96539cfe95ada05504f369244
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-03-25 09:30:07 +0900

    solve problem

--- 2292.py
+++ 2292.py
@@ -19,6 +19,7 @@ for i in range(2, 10):
 
 
 '''
+벌집수 숫자 크기
 6 2~7 hex = 2
 12 8~19 hex = 3
 18 20~37 hex = 4 .....
@@ -42,3 +43,11 @@ while not(str_end[0] <= n <= str_end[1]):
     str_end[0] = str_end[1] +1; str_end[1] += (current_hex-1)*6 
     print(str_end)
 print(current_hex)
+
+###########
+count = 1
+while 1 < n:
+    n -= count*6  # 시작값은 6씩 커지니까 값이 n이 -가 나오거나 1이 될 때 빠져나오면 바로 구할 수 있다. 
+    # 벌집 수는 6의 배수이고 시작숫자는 1이다. 그렇기에 6의 배수씩 뺐을 때 1 < n이 나오면 된다. 이동한 칸은 벌집 크기와 같기 때문에
+    count += 1
+print(count)
--- 2563.py
+++ 2563.py
@@ -1,8 +1,11 @@
 N = int(input())
 
-lst_ld = []
+interesting_x_y = []
 
 for i in range(N):
     left, down = map(int, input().split())
-    lst_ld.append((left, down))
-    
+    for i in range(left, left+10):
+        for k in range(down, down+10):
+            interesting_x_y.append((i, k)) #10 x 10 정사각형을 하나의 좌표로 1x1모양의 정사각형으로 모두 나타냄
+            # 1 1을 입력하면 (1, 1) (1, 2) (1, 3)... (1, 10) (2, 1) .... (10, 10) 이런식으로 한칸한칸을 모두 리스트에 저장
+print(len(set(interesting_x_y))) # 이중에서 좌표가 겹치는 부분을 교집합으로 합쳐줌 그리고 그 결과를 출력함
