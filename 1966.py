ba976583636e36a779b1ed62756e6292682a136c
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-05-25 15:58:22 +0900

    solve queue

--- 1966.py
+++ 1966.py
@@ -2,14 +2,31 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-# 구할 거를 저장해서 루프 돌리다가 만약 그게 바로 프린트 안되면 뒤로 옮기기
-stack = deque()
-n, m = 4, 2
-important = deque([1, 2, 3, 4])
-count = [0] * n
+for _ in range(int(input())):
+    # 구할 거를 저장해서 루프 돌리다가 만약 그게 바로 프린트 안되면 뒤로 옮기기
+    stack = deque()
+    n, m = map(int, input().split())
+    important = deque(list(map(int, input().split())))
+    count = [0] * n
 
-while len(important) > 1:
-    if important[0] != max(important):
-        important.append(important.popleft())
+    idx = m
+    pop = 0
+
+    if n == 1:
+        pop += 1
     else:
-        important.popleft()
+        while True:
+            
+
+            # print(important, idx)
+            if important[0] != max(important):
+                important.append(important.popleft())
+            else:
+                pop +=1
+                important.popleft()
+                if idx == 0:
+                    break
+            idx -= 1
+            if idx < 0:
+                idx = len(important) -1
+    print(pop)
