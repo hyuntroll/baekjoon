402c9b40eab0fcce5af76ac2ed3d9a26492ed153
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-05-29 21:47:26 +0900

    solve problem

--- 4948.py
+++ 4948.py
@@ -1,20 +1,25 @@
 import sys; input = sys.stdin.readline
-esa = [False, False] + [True]*123455
-for i in range(2, 123457):
-    if esa[i]:
-        for k in range(2*i, 123457, i):
-            esa[k] = False
-
+esa = [False, False] + [True]*246913 # 일단 0 , 1 제외 모든 수를 소수라 가정
+for i in range(2, 246913):
+    if esa[i]: # 숫자라 소수로 되어있다면
+        for k in range(2*i, 246913, i): # 그 수의 2배부터는 모두 다 소수가 아님 (i의 배수는 모두 소수가 아님)
+            esa[k] = False  # 그래서 i의 배수들은 모두 False처리
 
 
 while True:
-    n = int(input())
+    n = int(input().strip())
     if n == 0:
         break
     count = 0
-    for i in range(1, n+1):
+
+    for i in range(n+1, 2*n+1):
+        # print("a", i)
         if esa[i]:
             count += 1
+
+    print(count)
+
+
 #     count = 0
 #     for i in range(n+1, n*2+1):
 #         for k in range(2, int(i**(1/2))+1):
@@ -22,4 +27,3 @@ while True:
 #                 break
 #         else:
 #             count +=1
-    print(count)
