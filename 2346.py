58d637b39446db5b91124f47212823b4ed2f6a32
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-06-02 16:33:25 +0900

    solve 2346

---
+++ 2346.py
@@ -0,0 +1,44 @@
+from collections import deque
+import sys; input=sys.stdin.readline
+n = int(input())
+deq = list(map(int, input().split()))
+
+# n = 5
+# deq = [3, 2, 1, -3, -1]
+d_i = list(range(1, n+1))
+idx = 0
+
+
+def bob(i):
+    global deq, idx
+
+    idx += deq[i]
+    if deq[i] > 0:
+        idx -= 1
+
+    # print("i", deq.pop(i))
+    deq.pop(i)
+    print(d_i.pop(i), end=" ")
+    # print(deq.pop(i))
+
+    if len(deq) == 0:
+        return False
+
+    while True:
+        if idx < 0:
+            idx += len(deq)
+        elif idx >= len(deq):
+            idx -= len(deq)
+        else:
+            break
+    
+    # print("next:", idx)
+
+
+for i in range(n):
+    bob(idx)
+# bob(2)
+# bob(2)
+# bob(1)
+# bob(0)
+# print(deq)
