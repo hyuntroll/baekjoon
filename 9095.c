d55b02437de061ab6a14bca26427a7227cb7bdcc
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-09-04 23:15:29 +0900

    solve problem with c

--- 9095.c
+++ 9095.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int *dparr;
 
 int dp(int n) {
-    
+    if (1 <= n && n <= 3) { return dparr[n]; }
+    dparr[n] = dp(n-1) + dp(n-2) + dp(n-3);
+    return dparr[n];
 }
 
 int main(void) {
 
     int n;
+    int i;
     scanf("%d", &n);
+
+    dparr = (int * ) malloc(sizeof(int) * 12);
+    dparr[1] = 1; dparr[2] = 2; dparr[3] = 4;
     for (int i=0; i<n; i++) {
-
-
+        scanf("%d", &i);
+        printf("%d\n", dp(i));
     }
 
+    free(dparr);
 
     return 0;
 }
